bd8145228e42240416bf56f5d3cb980f4ffb0497
restuprtma <restu.prtma05@gmail.com>
2024-02-21 13:01:19 +0700

    Week 2 - Object

--- Jobsheet2/Buku09.java
+++ Jobsheet2/Buku09.java
@@ -7,6 +7,7 @@ public class Buku09 {
     void tampilInformasi(){
         System.out.println("Judul : " +judul);
         System.out.println("Pengarang : " +pengarang);
+        System.out.println("Jumlah halaman : " +halaman);
         System.out.println("Sisa Stock : " +stok);
         System.out.println("Harga : Rp " +harga);
     }
@@ -23,5 +24,17 @@ public class Buku09 {
 
     void gantiHarga(int hrg){
         harga = hrg;
+    }
+    
+    public Buku09(){
+
+    }
+
+    public Buku09(String jud, String pg, int hal, int stok, int har){
+        judul = jud;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
     }
 }
