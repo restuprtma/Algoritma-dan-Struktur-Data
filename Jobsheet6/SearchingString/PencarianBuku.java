14672886deee8c0fe7b74505177ee4147a932cb1
restuprtma <restu.prtma05@gmail.com>
2024-05-14 21:01:29 +0700

    Jobsheet 6 - Searching

---
+++ Jobsheet6/SearchingString/PencarianBuku.java
@@ -0,0 +1,79 @@
+package Jobsheet6.SearchingString;
+
+public class PencarianBuku {
+    Buku listBk[] = new Buku[5];
+    int idx;
+  
+    void tambah(Buku m) {
+      if (idx < listBk.length) {
+        listBk[idx] = m;
+        idx++;
+      } else {
+        System.out.println("Data sudah penuh");
+      }
+    }
+  
+    void tampil() {
+      for (Buku m : listBk) {
+        m.tampilDataBuku();
+      }
+    }
+  
+    public int FindSeqSearchStr(String cari) {
+      int posisi = -1;
+      for (int j = 0; j < listBk.length; j++) {
+        if (listBk[j].kodeBuku.equals(cari)) {
+          posisi = j;
+          break;
+        }
+      }
+      return posisi;
+    }
+  
+    public void Tampilposisi(String x, int pos) {
+      if (pos != -1) {
+        System.out.println("data : " + x + " ditemukan pada indeks " + pos);
+      } else {
+        System.out.println("data : " + x + " tidak ditemukan");
+      }
+    }
+  
+    public void TampilData(String x, int pos) {
+      if (pos != -1) {
+        System.out.println("Kode Buku \t : " + x);
+        System.out.println("Judul Buku \t : " + listBk[pos].judulBuku);
+        System.out.println("Tahun terbit \t : " + listBk[pos].tahunTerbit);
+        System.out.println("Pengarang \t : " + listBk[pos].pengarang);
+        System.out.println("Stock \t\t : " + listBk[pos].stock);
+      } else {
+        System.out.println("data " + x + " tidak ditemukan");
+      }
+    }
+
+    public Buku findBuku(String cari) {
+        Buku bukuFound = null;
+      
+        for (int i = 0; i < listBk.length; i++) {
+          if (listBk[i].kodeBuku.equals(cari)) {
+            bukuFound = listBk[i];
+            break;
+          }
+        }
+        return bukuFound;
+      }
+      
+      public int FindBinarySearchStr(String cari, int left, int right) {
+        while (right >= left) {
+          int mid = (right + left) / 2;
+          if (cari.equals(listBk[mid].kodeBuku)) {
+            return mid;
+          }
+          if (listBk[left].kodeBuku.compareTo(cari) <= 0 && cari.compareTo(listBk[mid].kodeBuku) <= 0) {
+            right = mid - 1;
+          } else {
+            left = mid + 1;
+          }
+        }
+        return -1;
+      }
+}
