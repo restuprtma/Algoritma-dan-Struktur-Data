517da60dbd561204e5d0964523b2d19f7809503f
restuprtma <restu.prtma05@gmail.com>
2024-05-22 19:57:05 +0700

    Jobsheet 8 - Queue

---
+++ Jobsheet8/Praktikum2/QueueMain.java
@@ -0,0 +1,65 @@
+package Jobsheet8.Praktikum2;
+import java.util.Scanner;
+
+public class QueueMain {
+    
+    public static void menu(){
+        System.out.println("Pilih menu: ");
+        System.out.println("1. Antrian baru");
+        System.out.println("2. Antrian keluar");
+        System.out.println("3. Cek Antrian terdepan");
+        System.out.println("4. Cek Semua antrian");
+        System.out.println("=========================");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan apasitas queue: ");
+        int jumlah = sc.nextInt();
+        Queue09 antri = new Queue09(jumlah);
+
+        int pilih;
+        do {
+            menu();
+            pilih = sc.nextInt();
+            sc.nextLine();
+            switch (pilih) {
+                case 1:
+                    System.out.print("\nNo Rekening \t: ");
+                    String norek = sc.nextLine();
+                    System.out.print("Nama \t\t: ");
+                    String nama = sc.nextLine();
+                    System.out.print("Alamat \t\t: ");
+                    String alamat = sc.nextLine();
+                    System.out.print("Umur \t\t: ");
+                    int umur = sc.nextInt();
+                    System.out.print("Saldo \t\t: ");
+                    double saldo = sc.nextDouble();
+
+                    Nasabah nasabah = new Nasabah(norek, nama, alamat, umur, saldo);
+                    sc.nextLine();
+                    antri.Enqueue(nasabah);
+                    break;
+
+                case 2:
+                    Nasabah data = antri.Dequeue();
+                    if (!"".equals(data.norek) && !"".equals(data.nama) && !"".equals(data.alamat) 
+                    && data.umur !=0 && data.saldo !=0) {
+                        System.out.println("Antrian yang keluar: " + data.norek + " " + data.nama + " "
+                        + data.alamat + " " + data.umur + " " + data.saldo);
+                        break;
+                    }
+                
+                case 3:
+                    antri.peek();
+                    break;
+
+                case 4:
+                    antri.print();
+                    break;
+            }
+        } while (pilih == 1 || pilih == 2 || pilih == 3 || pilih == 4);
+    } 
+
+    
+}
---
+++ Jobsheet8/Warung/Pembeli.java
@@ -0,0 +1,15 @@
+package Jobsheet8.Warung;
+
+public class Pembeli {
+    String nama;
+    int noHP;
+    
+    Pembeli(){
+
+    }
+    
+    Pembeli(String nama, int noHP){
+        this.nama = nama;
+        this.noHP = noHP;
+    }
+}
